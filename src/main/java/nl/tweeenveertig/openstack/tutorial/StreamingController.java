520981e6273e4fdfd1bd5f8e067a35e55558e343
opwvhk42 <oscar.westra@42.nl>
2012-09-25 15:53:48 +0200

    Implemented all use cases.

--- src/main/java/nl/tweeenveertig/openstack/tutorial/StreamingController.java
+++ src/main/java/nl/tweeenveertig/openstack/tutorial/StreamingController.java
@@ -1,14 +1,30 @@
 package nl.tweeenveertig.openstack.tutorial;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 
+import nl.tweeenveertig.openstack.client.Account;
+import nl.tweeenveertig.openstack.client.Container;
+import nl.tweeenveertig.openstack.client.StoredObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Controller to support streaming from &amp; to JOSS.
@@ -21,51 +37,185 @@ public class StreamingController {
     /**
      * Content type to use when none is given.
      */
-    static final String DEFAULT_BINARY_CONTENT_TYPE = "application/octet-stream";
+    public static final String DEFAULT_BINARY_CONTENT_TYPE = "application/octet-stream";
+    /**
+     * The storage provider we'll use. This logs in and provides us with the logged in account.
+     */
+    private StorageProvider storageProvider;
 
     /**
-     * Use case 1: stream content from storage to the browser.
+     * Create a streaming controller.
      *
-     * @param response the response to send the data to
+     * @param storageProvider the storage provider to use
+     */
+    @Autowired
+    public StreamingController(StorageProvider storageProvider) {
+
+        this.storageProvider = storageProvider;
+    }
+
+    /**
+     * Show the index page.
+     *
+     * @return a view for the index page
+     */
+    @RequestMapping("/")
+    public ModelAndView showIndexPage() {
+
+        Container container = getTutorialContainer();
+        StoredObject useCase2Object = container.getObject(StorageProvider.USE_CASE_2_OBJECT);
+        StoredObject useCase3Object1 = container.getObject(StorageProvider.USE_CASE_3_DEST_OBJECT_1);
+        StoredObject useCase3Object2 = container.getObject(StorageProvider.USE_CASE_3_DEST_OBJECT_2);
+
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("useCase2_ObjectExists", useCase2Object.exists());
+        model.put("useCase3_Object1Exists", useCase3Object1.exists());
+        model.put("useCase3_Object2Exists", useCase3Object2.exists());
+
+        return new ModelAndView("index", model);
+    }
+
+    private Container getTutorialContainer() {
+
+        Account account = storageProvider.getAccount();
+        return account.getContainer(StorageProvider.TUTORIAL_CONTAINER);
+    }
+
+    /**
+     * Use case 1: stream content from storage to the browser. This method also helps for the other use cases.
+     *
+     * @param objectName the name of the object to download
+     * @param response   the response to send the data to
      * @throws IOException when streaming the content fails
      */
-    @RequestMapping("download")
-    public void downloadContent(HttpServletResponse response) throws IOException {
+    @RequestMapping("/download/{objectName:.+}")
+    public void downloadContent(@PathVariable String objectName, HttpServletResponse response) throws IOException {
 
-        throw new UnsupportedOperationException("This operation is not yet implemented.");
+        // Get the object to download.
+
+        Container container = getTutorialContainer();
+        StoredObject storedObject = container.getObject(objectName);
+
+        if (storedObject.exists()) {
+
+            streamObject(storedObject, response);
+        } else {
+
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private void streamObject(StoredObject storedObject, HttpServletResponse response) throws IOException {
+
+        // Get the content type and the data stream.
+
+        String contentType = storedObject.getContentType();
+        if (contentType == null) {
+            contentType = DEFAULT_BINARY_CONTENT_TYPE;
+        }
+        InputStream dataStream = storedObject.downloadObjectAsInputStream();
+
+        // Stream the data.
+
+        OutputStream responseStream = null;
+        try {
+
+            response.setContentType(contentType);
+            responseStream = response.getOutputStream();
+            FileCopyUtils.copy(dataStream, responseStream);
+        } finally {
+
+            if (responseStream != null) {
+
+                responseStream.close();
+            }
+        }
     }
 
     /**
      * Use case 2: stream content from the browser to storage.
      *
      * @param file the file to stream
-     * @return the index page
+     * @return a redirect to the index page
      */
-    @RequestMapping("upload")
-    public ModelAndView uploadContent(@RequestParam MultipartFile file) {
+    @RequestMapping("/upload")
+    public View uploadContent(@RequestParam MultipartFile file) throws IOException {
 
-        throw new UnsupportedOperationException("This operation is not yet implemented.");
+        Container container = getTutorialContainer();
+
+        StoredObject newUpload = container.getObject(StorageProvider.USE_CASE_2_OBJECT);
+        storageProvider.streamUpload(newUpload, file.getInputStream());
+
+        return redirectToIndexPage();
+    }
+
+    private View redirectToIndexPage() {
+
+        return new RedirectView("/", true);
     }
 
     /**
      * Use case 3: generate content. This is the mean and bones of the use case.
      *
-     * @return the index page
+     * @return a redirect to the index page
      */
-    @RequestMapping(value = "generate", params = { "action=generate" })
-    public ModelAndView generateContent() {
+    @RequestMapping(value = "/generate")
+    public View generateContent() throws IOException {
 
-        throw new UnsupportedOperationException("This operation is not yet implemented.");
+        Container container = getTutorialContainer();
+
+        // Load the original image.
+
+        StoredObject source = container.getObject(StorageProvider.USE_CASE_3_SRC_OBJECT);
+        InputStream readingInputStream = source.downloadObjectAsInputStream();
+        BufferedImage originalImage = ImageIO.read(readingInputStream);
+
+        // Store as-is as first generated image.
+
+        StoredObject dest1 = container.getObject(StorageProvider.USE_CASE_3_DEST_OBJECT_1);
+        InputStream uploadSourceStream1 = createInputStream(originalImage);
+        storageProvider.streamUpload(dest1, uploadSourceStream1);
+
+        // Generate a thumbnail from it and store that as second generated image.
+
+        BufferedImage resizedImage = ImageUtils.scaleImage(originalImage, 200, 200);
+
+        StoredObject dest2 = container.getObject(StorageProvider.USE_CASE_3_DEST_OBJECT_2);
+        InputStream uploadSourceStream2 = createInputStream(resizedImage);
+        storageProvider.streamUpload(dest2, uploadSourceStream2);
+
+        return redirectToIndexPage();
+    }
+
+    private InputStream createInputStream(BufferedImage image) throws IOException {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", baos);
+
+        return new ByteArrayInputStream(baos.toByteArray());
     }
 
     /**
      * Use case 3: clear the generated content. This is a helper method.
      *
-     * @return the index page
+     * @return a redirect to the index page
      */
-    @RequestMapping(value = "generate", params = { "action=clear" })
-    public ModelAndView clearGeneratedContent() {
+    @RequestMapping(value = "/reset")
+    public View clearGeneratedContent() {
 
-        throw new UnsupportedOperationException("This operation is not yet implemented.");
+        Container container = getTutorialContainer();
+        deleteIfPresent(container, StorageProvider.USE_CASE_2_OBJECT);
+        deleteIfPresent(container, StorageProvider.USE_CASE_3_DEST_OBJECT_1);
+        deleteIfPresent(container, StorageProvider.USE_CASE_3_DEST_OBJECT_2);
+
+        return redirectToIndexPage();
+    }
+
+    private void deleteIfPresent(Container container, String objectName) {
+
+        StoredObject object1 = container.getObject(objectName);
+        if (object1.exists()) {
+            object1.delete();
+        }
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,63 +1,124 @@
+<%@ page import="nl.tweeenveertig.openstack.tutorial.StorageProvider" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
     <c:set var="path" value="${pageContext.request.contextPath}"/>
+    <style type="text/css">
+        body {
+            font-size: .9em;
+            margin: 0 auto;
+            max-width: 60em;
+        }
+
+        section {
+            border: 2px groove gray;
+            margin-bottom: 2em;
+            padding: 1em;
+        }
+
+        section h1 {
+            margin-top: 0;
+            padding-top: 0;
+            font-size: 1.5em;
+        }
+
+        section p.footnote {
+            border-top: thin dashed gray;
+            margin-top: 2em;
+            padding-top: 1em;
+            font-size: 80%;
+        }
+    </style>
     <link rel="icon" href="${path}/images/favicon.ico" type="image/x-icon"/>
-    <link rel="shortcut icon" href="${path}/images/favicon.ico" type="image/x-icon"/>
     <title>JOSS Tutorial - Streaming</title>
 </head>
 <body>
 
 <h1>JOSS Tutorial - Streaming</h1>
 
-
-<h2>Use case 1: downloading content</h2>
-
-<p>
-    This use case is about streaming content from the OpenStack Storage to the browser via your application. Although this is not a sensible approach for
-    public containers (you can then simply point the browser to the Storage directly), it <em>is</em> a common scenario for private containers. There are two
-    important reasons for this:
-</p>
-<ol>
-    <li>The security model is quite coarse grained: permissions are only set on a container.</li>
-    <li>Accessing a private contaier required an access token, which is always valid for 24 hours. You want to keep that private.</li>
-</ol>
-<p>
-    Start this use case (i.e. download a file) by clicking this link:
-    <a href="${path}/download">.../download</a>
-</p>
-
-
-<h2>Use case 2: uploading content</h2>
-
-<p>
-    [description of the use case]
-</p>
-<form method="post" action="${path}/upload" enctype="multipart/form-data">
+<section id="usecase1">
+    <h1>Use case 1: downloading content</h1>
     <p>
-        Start this use case submitting a file with the following form:
-
-        <label>File: <input type="file" name="file" size="60"/></label><br/>
-        <input type="submit" value="Upload"/>
+        This use case is about streaming content from the OpenStack Storage to the browser via your application. Although this is not a sensible approach for
+        public containers &mdash; you can then simply point the browser to the Storage directly &mdash; it <em>is</em> a common scenario for private containers.
     </p>
-</form>
-
-
-<h2>Use case 3: generating content</h2>
-
-<p>
-    [description of the use case]
-</p>
-<form method="post" action="${path}/generate">
     <p>
-        This use case can be started any way you like. Here, we use simple two-button form.
-
-        <button type="submit" name="action" value="generate">Generate</button>
-        &nbsp;&nbsp;&nbsp;
-        <button type="submit" name="action" value="clear">Clear</button>
+        As things stand now, there is no way to get an access token that is authorized only to read a specific container. If you have a token, you can do
+        anything. Even remove all containers. Needless to say, your token may not leave your application. Thus this use case.
     </p>
-</form>
+    <p>
+        Start this use case (i.e. download a file) by clicking this link:
+        <a href="${path}/download/<%=StorageProvider.USE_CASE_1_OBJECT%>">download</a>
+    </p>
+</section>
+
+<section id="usecase2">
+    <h1>Use case 2: uploading content</h1>
+    <p>
+        You cannot let a browser access stored content, nor can you stream content from storage, if there is no content. This use case is about the simple case
+        of providing content: streaming files (unchanged) directly into storage.
+    </p>
+    <p>
+        Start this use case by submitting a file with the following form:
+    </p>
+
+    <form method="post" action="${path}/upload" enctype="multipart/form-data">
+        <p>
+            <label>File: <input type="file" name="file" size="60"/></label><br/>
+            <input type="submit" value="Upload"/>
+        </p>
+    </form>
+
+    <%--@elvariable id="useCase2_ObjectExists" type="java.lang.Boolean"--%>
+    <c:if test="${useCase2_ObjectExists}">
+        <ul>
+            <li>View the uploaded object: <a href="${path}/download/<%=StorageProvider.USE_CASE_2_OBJECT%>">download</a></li>
+        </ul>
+    </c:if>
+
+    <p class="footnote">
+        There are two ways to get the file type attached to the object in our cloud storage: by file extension or manually. This tutorial uses the manual
+        method, by using &quot;MIME magic&quot;, i.e. pattern matching on the first 32 bytes of the file. For more information in the various options, you can
+        read <a href="http://www.rgagnon.com/javadetails/java-0487.html">Real's Howto</a>.
+    </p>
+</section>
+
+<section id="usecase3">
+    <h1>Use case 3: generating content</h1>
+    <p>
+        Sometimes, you want more than just pass along files. For example, you may want to generate thumbnails. For such a case it's usually a good idea to
+        store images in a queue somewhere (this may be a database). You can then retrieve and process them at your leasure. This use case demonstrates this for
+        a single image.
+    </p>
+
+    <form method="post" action="${path}/generate">
+        <p>
+            This use case can be started any way you like. Here, we use a button: <input type="submit" value="Generate"/>
+        </p>
+        <%--@elvariable id="useCase3_Object1Exists" type="java.lang.Boolean"--%>
+        <%--@elvariable id="useCase3_Object2Exists" type="java.lang.Boolean"--%>
+        <ul>
+            <li>View the source object: <a href="${path}/download/<%=StorageProvider.USE_CASE_3_SRC_OBJECT%>">download</a></li>
+            <c:if test="${useCase3_Object1Exists}">
+                <li>View the first generated object: <a href="${path}/download/<%=StorageProvider.USE_CASE_3_DEST_OBJECT_1%>">download</a></li>
+            </c:if>
+            <c:if test="${useCase3_Object2Exists}">
+                <li>View the second generated object: <a href="${path}/download/<%=StorageProvider.USE_CASE_3_DEST_OBJECT_2%>">download</a></li>
+            </c:if>
+        </ul>
+    </form>
+</section>
+
+<section id="reset">
+    <h1>Reset Data</h1>
+
+    <form method="post" action="${path}/reset">
+        <p>
+            This is not a usecase, but a button to clear all uploaded data: <input type="submit" value="Reset"/>
+        </p>
+    </form>
+</section>
 
 </body>
 </html>
